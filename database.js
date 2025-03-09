fa3c29d37857e1c473cc55775574e3f66505a3da
GalactikusExD <er610817@gmail.com>
2025-03-09 00:04:16 -0600

    Initial commit for backend

--- database.js
+++ database.js
@@ -4,8 +4,6 @@ require("dotenv").config();
 async function conectarDB() {
     try {
         await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
         });
         console.log("Conectado a MongoDB");
     } catch (error) {
